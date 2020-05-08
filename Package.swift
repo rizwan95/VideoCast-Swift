b227eb868e87cf49a05d6988f9d4d6479d376e42
Rizwan Ahmed <rizwan.ahmed95@gmail.com>
2020-05-08 17:43:47 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,3 @@
-//
-//  File.swift
-//  VideoCast
-//
-//  Created by Tomohiro Matsuzawa on 2018/03/16.
-//  Copyright © 2018年 CyberAgent, Inc. All rights reserved.
-//
-
 // swift-tools-version:5.2
 
 import PackageDescription
